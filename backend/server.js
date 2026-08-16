55c85f902eb569a934abb3eb10e6f3c46b06393f
Nhluvuko Baloyi <nhluvukot9@gmail.com>
2026-08-16 18:24:00 +0200

    Initial commit after setting up backend servers

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+console.log("Port value is:", process.env.PORT);
+console.log("URI exist?", process.env.MONGODB_URI? "Yes" : "NO");
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Server running smoothly!')
+})
+
+// Connecting to MongoDB 
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas!');
+    })
+    .catch ((error) => {
+        console.error('Connection to MongoDB Atlas failed:', error);
+    });
+
+// Server starter
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
