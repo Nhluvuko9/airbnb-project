00666a6b694ba91d7d5945dc0186c50ed137a33f
Nhluvuko Baloyi <nhluvukot9@gmail.com>
2026-08-19 22:48:05 +0200

    Browser routes for different pages configured and login input form created with functions to check format

---
+++ frontend/src/pages/CreateListings.jsx
@@ -0,0 +1,9 @@
+const CreateListings = () => {
+    return (
+        <div className="page-container">
+            <h2>Create a new listing</h2>
+        </div>
+    )   
+}
+
+export default CreateListings;
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+const Dashboard = () => {
+    return (
+        <div className="page-container">
+            <h2>Dashboard</h2>
+        </div>
+
+    )
+
+   
+}
+
+export default Dashboard;
